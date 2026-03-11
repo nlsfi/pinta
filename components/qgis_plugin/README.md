0a54b75bec43565e0d075433da654b50857303c1
vaahtokarkki <roni.lindholm@gmail.com>
2026-03-11 09:58:01 +0000

    feat: add initial qgis plugin implementation

---
+++ components/qgis_plugin/README.md
@@ -0,0 +1,15 @@
+# Development of Pinta QGIS plugin
+
+## Setting up development environment
+
+* Make sure you have opened project in devcontainer
+* Activate the root venv
+* Create a `.env` from `.env.example`, change configurations if needed
+* Launch development QGIS: `qpdt s` from devcontainer shell
+
+## Translating with QT Linguistic
+
+The translation files are in [i18n](./src/pinta/resources/i18n) folder. Translatable
+content in python files is code such as `tr(u"Hello World")`.
+
+Translation files can be updated with [updte_translations.sh](./src/updte_translations.sh) script. After updating ts files, you can open file you wish to translate with Qt Linguist or code editor, make the changes and compile the translations to .qm files using [compile_translations.sh](./src/compile_translations.sh) script.
---
+++ components/qgis_plugin/src/pinta_qgis_plugin/__init__.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2026 Pinta QGIS Plugin Contributors.
+#
+#
+# This file is part of Pinta QGIS Plugin.
+#
+# Pinta QGIS Plugin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pinta QGIS Plugin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pinta QGIS Plugin.  If not, see <https://www.gnu.org/licenses/>.
+
+import typing
+
+from qgis.utils import plugins
+
+from pinta_qgis_plugin.utils import i18n_utils
+
+if typing.TYPE_CHECKING:
+    from pinta_qgis_plugin.plugin import Plugin
+
+__version__ = "0.0.0"
+
+
+def classFactory(_):  # noqa: ANN201, ANN001, N802
+    """Class factory."""
+    translators = i18n_utils.setup_all_translators()  # noqa: F841 (has to be assigned)
+
+    from pinta_qgis_plugin.plugin import Plugin  # noqa: PLC0415
+
+    return Plugin()
+
+
+def get_instance() -> "Plugin | None":
+    """Get instance."""
+    return plugins.get(__name__)
