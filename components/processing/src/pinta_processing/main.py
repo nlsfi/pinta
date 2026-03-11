118aaa147bd449d1ddca4bc12a470019100fd2e8
vaahtokarkki <roni.lindholm@gmail.com>
2026-03-11 12:03:25 +0000

    feat: add processing component

---
+++ components/processing/src/pinta_processing/main.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2026 National Land Survey of Finland
+# (https://www.maanmittauslaitos.fi/en).
+# This file is part of the Pinta.
+# Licensed under the MIT License; see the repository LICENSE file.
+
+
+def main() -> None:  # noqa: D103
+    print("Hello from processing!")  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()
