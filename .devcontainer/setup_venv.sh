0ad4daa2690f959058d8fcfaa7267f22e6b5159d
Joonalai <joona.laine@cofactor.fi>
2026-03-16 10:33:06 +0200

    chore: use alembic as a migration extra dependency
    chore: use alembic as a "migrations" extra dependency

--- .devcontainer/setup_venv.sh
+++ .devcontainer/setup_venv.sh
@@ -5,5 +5,5 @@ COMPONENTS_DIR="components"
 
 uv venv --system-site-packages --clear
 source .venv/bin/activate
-uv sync --all-packages --all-groups
+uv sync --all-packages --all-groups --all-extras
 prek install
