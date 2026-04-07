c97aace3d172590f02ed26f1c7886025523ff272
vaahtokarkki <roni.lindholm@gmail.com>
2026-04-07 12:56:44 +0000

    chore: refine devcontainer setup to use caches

--- .devcontainer/setup_venv.sh
+++ .devcontainer/setup_venv.sh
@@ -3,7 +3,11 @@ set -e
 
 COMPONENTS_DIR="components"
 
-uv venv --system-site-packages --clear
+
+if [ ! -d .venv ]; then
+	uv venv --system-site-packages --clear .venv
+fi
+
 source .venv/bin/activate
 # Cache (most) of the dependencies
 uv sync --all-packages --all-groups --all-extras --no-extra qgis --no-extra build
