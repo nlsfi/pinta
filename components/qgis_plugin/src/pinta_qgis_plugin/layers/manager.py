8483692bc5ba25e9027246885c7ceba28f188cd8
Joonalai <joona.laine@cofactor.fi>
2026-04-07 09:25:19 +0300

    refactor: move vector layer creation to its own module [MATI-6586]

--- components/qgis_plugin/src/pinta_qgis_plugin/layers/manager.py
+++ components/qgis_plugin/src/pinta_qgis_plugin/layers/manager.py
@@ -18,52 +18,14 @@
 
 import logging
 
-from qgis.core import QgsDataSourceUri, QgsProject, QgsVectorLayer
 from qgis_plugin_tools.tools.decorations import log_if_fails
 
-from pinta_qgis_plugin.exceptions import LayerCreationError
-from pinta_qgis_plugin.layers import config, database
-from pinta_qgis_plugin.layers.config import LayerConfig
+from pinta_qgis_plugin.layers import vector_layer
 
 LOGGER = logging.getLogger(__name__)
-
-PROVIDER_LIB = "postgres"
-
-
-def _create_qgs_vector_layer(uri: QgsDataSourceUri, name: str) -> QgsVectorLayer:
-    return QgsVectorLayer(uri.uri(), name, PROVIDER_LIB)
-
-
-def create_layer(config: LayerConfig) -> QgsVectorLayer:
-    """Create Production areas layer from database."""
-    uri = database.get_database_uri()
-
-    schema = config.db_model.__table_args__.get("schema")
-    table_name = config.db_model.__tablename__
-    uri.setDataSource(schema, table_name, config.geom_column)
-    uri.setKeyColumn(config.key_column)
-    uri.setWkbType(config.wkb_type)
-    uri.setSrid(config.srid)
-
-    layer = _create_qgs_vector_layer(uri, config.layer_name)
-
-    if not layer.isValid():
-        raise LayerCreationError(config.layer_name)
-
-    layer.setReadOnly(True)
-
-    return layer
 
 
 @log_if_fails
 def initialize_layers() -> None:
     """Initialize and load all layers into QGIS project."""
-    for layer_config in config.LAYERS:
-        layer_name = layer_config.layer_name
-        layer = create_layer(layer_config)
-
-        added_layer = QgsProject.instance().addMapLayer(layer, addToLegend=True)
-        if added_layer is not None:
-            LOGGER.info("%s layer loaded successfully", layer_name)
-        else:
-            raise LayerCreationError(layer_name)
+    vector_layer.add_vector_layers()
--- components/qgis_plugin/test_qgis/layers/test_manager.py
+++ components/qgis_plugin/test_qgis/layers/test_manager.py
@@ -20,112 +20,21 @@ from unittest.mock import MagicMock
 
 import pytest
 from pytest_mock import MockerFixture
-from qgis.core import QgsVectorLayer
 
-from pinta_qgis_plugin.layers import config, manager
-
-
-@pytest.fixture
-def mock_uri():
-    uri = MagicMock()
-    uri.uri.return_value = "postgres://test"
-    return uri
+from pinta_qgis_plugin.layers import manager, vector_layer
 
 
 @pytest.fixture
-def fake_layer(mocker: MockerFixture, mock_uri: MagicMock) -> QgsVectorLayer:
-    fake_layer = QgsVectorLayer("MultiPolygon", "empty", "memory")
-    mocker.patch.object(
-        manager.database,
-        "get_database_uri",
+def mock_add_vector_layers(mocker: MockerFixture) -> MagicMock:
+    return mocker.patch.object(
+        vector_layer,
+        "add_vector_layers",
         autospec=True,
-        return_value=mock_uri,
     )
-    mocker.patch.object(
-        manager,
-        "_create_qgs_vector_layer",
-        autospec=True,
-        return_value=fake_layer,
-    )
-    return fake_layer
-
-
-@pytest.fixture
-def mock_qgs_project(mocker: MockerFixture) -> MagicMock:
-    mock_project = MagicMock()
-    mocker.patch.object(
-        manager.QgsProject,
-        "instance",
-        autospec=True,
-        return_value=mock_project,
-    )
-    return mock_project
 
 
 def test_create_layer_with_valid_layer_returns_layer(
-    mock_uri: MagicMock,
-    fake_layer: QgsVectorLayer,
+    mock_add_vector_layers: MagicMock,
 ):
-    result = manager.create_layer(config.PRODUCTION_AREA)
-
-    assert result is fake_layer
-    mock_uri.setDataSource.assert_called_once_with(
-        "management", "production_area", "geom"
-    )
-    mock_uri.setKeyColumn.assert_called_once_with("id")
-    mock_uri.setWkbType.assert_called_once()
-    mock_uri.setSrid.assert_called_once_with("3067")
-    assert fake_layer.readOnly
-
-
-def test_create_layer_with_invalid_layer_raises_exception(
-    mocker: MockerFixture,
-):
-    mock_layer = MagicMock()
-    mock_layer.isValid.return_value = False
-    mocker.patch.object(
-        manager,
-        "_create_qgs_vector_layer",
-        autospec=True,
-        return_value=mock_layer,
-    )
-
-    with pytest.raises(manager.LayerCreationError):
-        manager.create_layer(config.PRODUCTION_AREA)
-
-    mock_layer.setReadOnly.assert_not_called()
-
-
-def test_initialize_layers_with_valid_layer_adds_to_project(
-    mocker: MockerFixture,
-    mock_qgs_project: MagicMock,
-):
-    mock_layer = MagicMock(spec=QgsVectorLayer)
-    mocker.patch.object(
-        manager,
-        "create_layer",
-        autospec=True,
-        return_value=mock_layer,
-    )
-
     manager.initialize_layers()
-
-    assert mock_qgs_project.addMapLayer.call_count == len(config.LAYERS)
-    mock_qgs_project.addMapLayer.assert_called_with(mock_layer, addToLegend=True)
-
-
-def test_initialize_layers_with_exception_does_not_add_to_project(
-    mocker: MockerFixture,
-    mock_qgs_project: MagicMock,
-):
-    mocker.patch.object(
-        manager,
-        "create_layer",
-        autospec=True,
-        side_effect=manager.LayerCreationError("test_layer"),
-    )
-
-    manager.initialize_layers()
-
-    assert manager.create_layer.call_count == 1  # type: ignore[attr-defined]
-    mock_qgs_project.addMapLayer.assert_not_called()
+    mock_add_vector_layers.assert_called_once()
