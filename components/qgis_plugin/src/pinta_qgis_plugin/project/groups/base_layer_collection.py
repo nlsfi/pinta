8de45d0ddc20435dc3a7884dee837ac03a160ef6
vaahtokarkki <roni.lindholm@gmail.com>
2026-05-19 06:34:34 +0000

    refactor(qgis): allow adding layer collections to groups [MATI-6908]

--- components/qgis_plugin/src/pinta_qgis_plugin/project/groups/base_layer_collection.py
+++ components/qgis_plugin/src/pinta_qgis_plugin/project/groups/base_layer_collection.py
@@ -19,22 +19,27 @@
 import abc
 import logging
 import typing
-from typing import Optional
 
-from qgis.core import QgsProject, QgsRasterLayer, QgsVectorLayer
+from qgis.core import QgsLayerTreeGroup, QgsProject, QgsRasterLayer, QgsVectorLayer
 
 from pinta_qgis_plugin.exceptions import LayerCreationError
 
 LOGGER = logging.getLogger(__name__)
+
+GROUP_CUSTOM_PROPERTY_ID = "pinta_layer_group_id"
 
 
 class BaseLayerCollection(abc.ABC):
     """Base class for layer collections."""
 
     COLLECTION_ID_KEY = "collection_id"
+    collection_id: str | None = None
 
-    collection_id: str
-    _instance: Optional["BaseLayerCollection"] = None
+    # Set this to true on subclasses if layers are added
+    # to the group instead of the root of the layer tree
+    add_layers_to_group: bool = False
+
+    _instance: typing.Optional["BaseLayerCollection"] = None
 
     @classmethod
     def get(cls) -> "BaseLayerCollection":
@@ -42,6 +47,43 @@ class BaseLayerCollection(abc.ABC):
         if cls._instance is None:
             cls._instance = cls()
         return typing.cast("BaseLayerCollection", cls._instance)
+
+    def find_group_from_tree(self) -> QgsLayerTreeGroup | None:
+        """Find the group of the collection from the layer tree."""
+        all_groups = QgsProject.instance().layerTreeRoot().findGroups(recursive=True)
+        return next(
+            (
+                group
+                for group in all_groups
+                if group.customProperty(GROUP_CUSTOM_PROPERTY_ID) == self.collection_id
+            ),
+            None,
+        )
+
+    def add_group_to_project(self) -> QgsLayerTreeGroup:
+        """Add a group to the project."""
+        group = self.find_group_from_tree()
+        if group is not None:
+            return group
+
+        if self.collection_id is None:
+            msg = "Collection ID is not set for the layer collection."
+            raise LayerCreationError(msg)
+
+        root = QgsProject.instance().layerTreeRoot()
+        group = root.insertGroup(0, self.collection_id)
+        group.setCustomProperty(GROUP_CUSTOM_PROPERTY_ID, self.collection_id)
+        return group
+
+    def remove_group_from_tree(self) -> None:
+        """Remove the group of the collection from the layer tree."""
+        if (group := self.find_group_from_tree()) is not None:
+            group.removeChildrenGroupWithoutLayers()
+            if len(group.children()) == 0:
+                parent_group = typing.cast("QgsLayerTreeGroup", group.parent())
+                parent_group.removeChildNode(group)
+            else:
+                group.removeCustomProperty(GROUP_CUSTOM_PROPERTY_ID)
 
     def add_to_project(self) -> None:
         """Add layers to the project."""
@@ -57,6 +99,7 @@ class BaseLayerCollection(abc.ABC):
         """Remove layers from the project."""
         for layer in self.find_layers():
             QgsProject.instance().removeMapLayer(layer)
+        self.remove_group_from_tree()
 
     def find_layers(self) -> list[QgsVectorLayer | QgsRasterLayer]:
         """Return layers in the collection."""
@@ -68,8 +111,9 @@ class BaseLayerCollection(abc.ABC):
 
     def _add_map_layer_to_project(self, layer: QgsVectorLayer | QgsRasterLayer) -> None:
         layer.setCustomProperty(self.COLLECTION_ID_KEY, self.collection_id)
-
-        added_layer = QgsProject.instance().addMapLayer(layer, addToLegend=True)
+        added_layer = QgsProject.instance().addMapLayer(
+            layer, addToLegend=not self.add_layers_to_group
+        )
         if added_layer is not None:
             LOGGER.debug("%s layer loaded successfully", layer.name())
         else:
