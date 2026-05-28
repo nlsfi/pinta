54be966f0267d055442090582d52a6d16c37fd9b
Joonalai <joona.laine@cofactor.fi>
2026-05-28 15:57:34 +0300

    chore(db): ensure all db deps ship with backend [MATI-6635]

--- components/db/src/pinta_db_utils/model_utils.py
+++ components/db/src/pinta_db_utils/model_utils.py
@@ -8,18 +8,6 @@
 from geoalchemy2 import Geometry
 from sqlmodel import SQLModel
 
-from pinta_db.exceptions import MissingSchemaError
-
-
-def foreign_key(model_class: type[SQLModel], field_name: str = "id") -> str:
-    """Generate foreign key string representation."""
-    table_name = model_class.__tablename__
-    table_args = model_class.__table_args__ or {}  # type: ignore[attr-defined]
-    if not (schema := table_args.get("schema")):
-        raise MissingSchemaError(model_class)
-
-    return f"{schema}.{table_name}.{field_name}"
-
 
 def geometry_type(model_class: type[SQLModel], field_name: str = "geom") -> str:
     """Get geometry type from SQLModel class."""
